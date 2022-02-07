b3785c191b129d8615a00b48568377d88a65a685
ZenxMato <97756244+ZenxMato@users.noreply.github.com>
2022-02-08 06:03:43 +0700

    Update __main__.py

--- Yukki/__main__.py
+++ Yukki/__main__.py
@@ -87,7 +87,7 @@ async def initiate_bot():
     try:
         await app.send_message(
             LOG_GROUP_ID,
-            "__✅Bot Music Sudah Online__\n\n<b>[「Contact Owner」](https://t.me/zenzuzu2)</b>",
+            "__✅Bot Music Sudah Online__",
         )
     except Exception as e:
         print(
@@ -106,7 +106,7 @@ async def initiate_bot():
         try:
             await ASS_CLI_1.send_message(
                 LOG_GROUP_ID,
-                "__✅Asisten Sudah Online__\n\n<b>[「Contact Owner」](https://t.me/zenzuzu2)</b>",
+                "__✅Asisten Sudah Online__",
             )
         except Exception as e:
             print(
